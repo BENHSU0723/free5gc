6a28d3df66164ef01a9e260ff7da799106aa9f76
ben hsu <ben0978327139@gmail.com>
2024-08-17 21:16:09 +0800

    feat(5glan): a stable 5glan version of control plane
    Signed-off-by: ben hsu <ben0978327139@gmail.com>

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+sudo iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
+sudo systemctl stop ufw
+sudo systemctl disable ufw # prevents the firewall to wake up after a OS reboot
