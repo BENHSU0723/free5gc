64488a8837ef0ef4866f41b5676791bddb428164
ben hsu <ben0978327139@gmail.com>
2024-10-31 15:19:42 +0800

    tmp commit for new vm

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
-sudo iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
+# sudo iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
+sudo iptables -I FORWARD 1 -j ACCEPT
 sudo systemctl stop ufw
 sudo systemctl disable ufw # prevents the firewall to wake up after a OS reboot
